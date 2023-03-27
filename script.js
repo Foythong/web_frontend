90f7abc3162e17b1fb143ecfbbab0c66582c7b54
Foythong <103359266+Foythong@users.noreply.github.com>
2023-03-27 23:46:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function img(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("img-demo");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+//   for (i = 0; i < dots.length; i++) {
+//      dots[i].className = dots[i].className = ("img-demo", ".img-demo-red");
+//   }
+  x[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += "img-demo";
+}
